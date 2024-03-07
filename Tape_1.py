4a3a441edd9ad38dfb3967bf02e953ef48dcbc83
JosephOziel <joziel848@gmail.com>
2024-03-07 16:53:55 -0500

    stuff

--- Tape_1.py
+++ Tape_1.py
@@ -5,18 +5,24 @@ import os
 Tape = MIDIFile(2)
 
 piano = 0
-rh = 0
-lh = 1
+one = 0
+two = 1
+three = 2
+
 tempo1 = 169
 
 time = 0
-time2 = 15  
+time2 = 25
+time3 = 65
 
 volume = 90
 
-Tape.addTempo(piano, 0, tempo1)
+Tape.addTempo(piano, one, tempo1)
+Tape.addProgramChange(piano, one, time, 1)
+Tape.addProgramChange(piano, two, time, 1)
 
 intervals = [(pi/8)*x for x in range(0,16)]
+intervals2 = [(pi/8)*x for x in range(62)]
 
 def calc_sin_pitch(x,a):
     return ceil(44*sin(x)+a)
@@ -25,21 +31,24 @@ def calc_cos_pitch(x,a):
     return ceil(44*cos(x)+a)
 
 def calc_sin_divx_pitch(x,a):
-    return ceil(44*(sin(x)/x)+a)
+    if x == 0:
+        return a
+    else:
+        return ceil(46*(sin(x)/x)+a)
 
 for _ in range(20):
-    volume = volume + 3
     for x in intervals:
-        Tape.addNote(piano, rh, calc_sin_pitch(x, 44), time, 0.3333, volume)
+        Tape.addNote(piano, one, calc_sin_pitch(x, 56), time, 0.3333, volume)
         time = time + 0.5
 
-volume = 90
-
 for _ in range(15):
-    volume = volume + 4
     for x in intervals:
-        Tape.addNote(piano, lh, calc_cos_pitch(x, 45), time2, 0.7, volume)
+        Tape.addNote(piano, two, calc_cos_pitch(x, 57), time2, 0.7, volume)
         time2 = time2 + 0.7
+
+for x in intervals2:
+    Tape.addNote(piano, three, calc_sin_divx_pitch(x, 10), time3, 1.1, volume)
+    time3 = time3 + 1.1
 
 os.chdir('C:\\Users\\vatis\\OneDrive\\Documents\\Midi_Tapes')
 with open('Tape_1.mid', 'wb') as output_file:
