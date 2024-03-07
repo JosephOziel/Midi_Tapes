c25647572645320364709da934f33b48105fb089
crab <92041779+crabbo-rave@users.noreply.github.com>
2024-03-06 20:47:13 -0500

    stuff

--- Tape_1.py
+++ Tape_1.py
@@ -1,3 +1,28 @@
-from midiutil import MIDFile
+from midiutil import MIDIFile
 from math import *
+import os
 
+Tape = MIDIFile(2)
+
+piano = 0
+rh = 0
+lh = 1
+tempo1 = 169
+
+time = 0
+
+Tape.addTempo(piano, 0, tempo1)
+
+sinpitches = [(pi/8)*x for x in range(0,16)]
+
+def calc_sin_pitch(x):
+    return ceil(44*sin(x)+44)
+
+for _ in range(20):
+    for x in sinpitches:
+        Tape.addNote(piano, rh, calc_sin_pitch(x), time, 0.3333, 100)
+        time = time + 0.5
+
+os.chdir('C:\\Users\\vatis\\OneDrive\\Documents\\Midi_Tapes')
+with open('Tape_1.mid', 'wb') as output_file:
+    Tape.writeFile(output_file)
