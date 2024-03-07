bb82cfecfe09284215773c3863e30c4b66a8fbe4
JosephOziel <joziel848@gmail.com>
2024-03-07 11:01:38 -0500

    stuff

--- Tape_1.py
+++ Tape_1.py
@@ -12,7 +12,7 @@ tempo1 = 169
 time = 0
 time2 = 15  
 
-volume = 100
+volume = 90
 
 Tape.addTempo(piano, 0, tempo1)
 
@@ -20,19 +20,23 @@ intervals = [(pi/8)*x for x in range(0,16)]
 
 def calc_sin_pitch(x,a):
     return ceil(44*sin(x)+a)
+
 def calc_cos_pitch(x,a):
     return ceil(44*cos(x)+a)
 
+def calc_sin_divx_pitch(x,a):
+    return ceil(44*(sin(x)/x)+a)
+
 for _ in range(20):
-#    volume = volume - 3
+    volume = volume + 3
     for x in intervals:
         Tape.addNote(piano, rh, calc_sin_pitch(x, 44), time, 0.3333, volume)
         time = time + 0.5
 
-volume = 100
+volume = 90
 
 for _ in range(15):
-#    volume = volume - 4
+    volume = volume + 4
     for x in intervals:
         Tape.addNote(piano, lh, calc_cos_pitch(x, 45), time2, 0.7, volume)
         time2 = time2 + 0.7
