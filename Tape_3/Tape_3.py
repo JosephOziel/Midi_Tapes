5babd88dc8a06c1737ca3266b52bcd5370bdff8f
crab <joziel848@gmail.com>
2024-03-15 21:38:11 -0400

    commit

--- Tape_3/Tape_3.py
+++ Tape_3/Tape_3.py
@@ -10,7 +10,7 @@ seq = [1,1,4,8,2,8,4,12,3,1,12,24,2,16,8,24,3,21,7,27,
 seq2 = [abs(v-35) for v in seq]
 seq3 = [(v+35) for v in reversed(seq)]
 
-Tape = MIDIFile(1)
+Tape = MIDIFile(2)
 
 Tape.addTempo(0, 0, 108.88)
 
@@ -50,6 +50,37 @@ time = time + 2.2
 for v in seq3:
     Tape.addProgramChange(0, 0, time, (v%128)+1)
     Tape.addNote(0, 0, (v%88)+1, time, duration(v), (v%50)+50)
+    time = time + duration(v)
+
+# A fractal array resembling the shape of a conifer tree read by rows. A mirror symmetric array of numbers where the n-th term is equal to the number of terms in the n-th row of the array.
+cseq = [1,2,3,4,1,5,6,2,3,7,8,9,4,1,5,10,11,6,2,3,7,12,
+ 13,14,15,8,16,17,9,4,1,5,10,18,19,11,6,2,3,7,12,
+ 20,21,13,8,4,1,5,9,14,22,23,15,16,24,25,26,17,10,
+ 18,27,28,19,11,6,2,3,7,12,20,29,30,21,13,8]
+cseq2 = [(x+45) for x in reversed(cseq)]
+cseq3 = [(88-(x+21)) for x in cseq]
+
+time = 50
+
+Tape.addTempo(1, 50, 123.45)
+
+for v in [(x+25) for x in cseq]:
+    Tape.addProgramChange(1, 1, time, (v%128)+1)
+    Tape.addNote(1, 1, (v%88)+1, time, duration(v), (v%50)+50)
+    time = time + duration(v)
+
+time = time + 2.3
+
+for v in cseq2:
+    Tape.addProgramChange(1, 1, time, (v%128)+1)
+    Tape.addNote(1, 1, (v%88)+1, time, duration(v), (v%50)+50)
+    time = time + duration(v)
+
+time = time + 2.4
+
+for v in cseq3:
+    Tape.addProgramChange(1, 1, time, (v%128)+1)
+    Tape.addNote(1, 1, (v%88)+1, time, duration(v), (v%50)+50)
     time = time + duration(v)
 
 os.chdir('C:\\Users\\vatis\\OneDrive\\Documents\\Midi_Tapes\\Tape_3')
