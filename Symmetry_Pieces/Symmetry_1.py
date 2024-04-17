7995107d76b9058549384158b141515d9fcd70c1
crab <joziel848@gmail.com>
2024-04-16 22:55:08 -0400

    Create Symmetry_1.py

---
+++ Symmetry_Pieces/Symmetry_1.py
@@ -0,0 +1,12 @@
+from midiutil import *
+from math import *
+import os
+from random import *
+
+Tape = MIDIFile(2)
+
+# function to use: f(x)=sin(x)^1/3+cos(x)^1/5
+
+os.chdir('C:\\Users\\vatis\\OneDrive\\Documents\\Midi_Tapes\\Symmetry_Pieces')
+with open('Symmetry_1.mid', 'wb') as output_file:
+    Tape.writeFile(output_file)
