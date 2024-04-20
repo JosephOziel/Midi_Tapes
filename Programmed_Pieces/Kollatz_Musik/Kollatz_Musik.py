5cfe50db3e90436215acae7fdbb280514198579d
crab <joziel848@gmail.com>
2024-04-20 12:03:50 -0400

    x

--- Programmed_Pieces/Kollatz_Musik/Kollatz_Musik.py
+++ Programmed_Pieces/Kollatz_Musik/Kollatz_Musik.py
@@ -8,5 +8,5 @@ Tape = MIDIFile(2)
 # function to use: f(x)=sin(x)^1/3+cos(x)^1/5
 
 os.chdir('C:\\Users\\vatis\\OneDrive\\Documents\\Midi_Tapes\\Symmetry_Pieces')
-with open('Symmetry_1.mid', 'wb') as output_file:
+with open('Kollatz_Musik.mid', 'wb') as output_file:
     Tape.writeFile(output_file)
