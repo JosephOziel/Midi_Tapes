67184c1c43952177841257c79b79ef6725bd8461
JosephOziel <joziel848@gmail.com>
2024-04-25 23:10:21 -0400

    d

--- Programmed_Pieces/Kollatz_Musik/Kollatz_Musik.py
+++ Programmed_Pieces/Kollatz_Musik/Kollatz_Musik.py
@@ -5,9 +5,6 @@ from random import *
 
 T = MIDIFile(1) #maybe 2 channels later?
 
-temp = 169 # subject to change
-T.addTempo(0,0,temp)
-
 T.addProgramChange(0,0,0,1)
 T.addProgramChange(0,1,0,1)
 T.addProgramChange(0,2,0,1)
@@ -61,77 +58,103 @@ def kol2(s, h):
     time = time + 1.5
 
 #symmetry
-def kol3(s, h1, h2, i):
+def kol3(s, h1, h2, i, ch):
     global time
-    T.addNote(0,0,val(s+o),time,dur/2,100)
-    T.addNote(0,0,val(s+o+h1),time,dur/2,100)
-    T.addNote(0,0,val(abs(i-((s+o)-i))),time,dur/2,100)
-    T.addNote(0,0,val(abs((i-((s+o)-i))+h2)),time,dur/2,100)
+    T.addNote(0,ch,val(s+o),time,dur/2,100)
+    T.addNote(0,ch,val(s+o+h1),time,dur/2,100)
+    T.addNote(0,ch,val(abs(i-((s+o)-i))),time,dur/2,100)
+    T.addNote(0,ch,val(abs((i-((s+o)-i))+h2)),time,dur/2,100)
     time = time + dur
     while s != 1:
         if (s%2)==0:
             s = s//2
-            T.addNote(0,0,val(s+o),time,dur/2,100)
-            T.addNote(0,0,val(s+o+h1),time,dur/2,100)
-            T.addNote(0,0,val(abs(i-((s+o)-i))),time,dur/2,100)
-            T.addNote(0,0,val(abs((i-((s+o)-i))+h2)),time,dur/2,100)
+            T.addNote(0,ch,val(s+o),time,dur/2,100)
+            T.addNote(0,ch,val(s+o+h1),time,dur/2,100)
+            T.addNote(0,ch,val(abs(i-((s+o)-i))),time,dur/2,100)
+            T.addNote(0,ch,val(abs((i-((s+o)-i))+h2)),time,dur/2,100)
             time = time + dur
         else:
             s = s*3+1
-            T.addNote(0,0,val(s+o),time,dur/2,100)
-            T.addNote(0,0,val(s+o+h1),time,dur/2,100)
-            T.addNote(0,0,val(abs(i-((s+o)-i))),time,dur/2,100)
-            T.addNote(0,0,val(abs((i-((s+o)-i))+h2)),time,dur/2,100)
+            T.addNote(0,ch,val(s+o),time,dur/2,100)
+            T.addNote(0,ch,val(s+o+h1),time,dur/2,100)
+            T.addNote(0,ch,val(abs(i-((s+o)-i))),time,dur/2,100)
+            T.addNote(0,ch,val(abs((i-((s+o)-i))+h2)),time,dur/2,100)
             time = time + dur
     time = time + 1.5
 
+#maybe add another version with harmony? FIXX
 def kol4(s, h, r, cha):
-    global time2
-    T.addNote(0,cha,val(s+o),time2,r,100)
-    T.addNote(0,cha,val(s+o+h),time2,r,100)
-    time2 = time2 + r
+    global time3, time2
+    T.addNote(0,cha,val(s+o),time3,r,100)
+    T.addNote(0,cha,val(s+o+h),time3,r,100)
+    time3 = time3 + r
     while s != 1:
         if (s%2)==0:
             s = s//2
-            T.addNote(0,cha,val(s+o),time2,r,100)
-            T.addNote(0,cha,val(s+o+h),time2,r,100)
-            time2 = time2 + r
+            T.addNote(0,cha,val(s+o),time3,r,100)
+            T.addNote(0,cha,val(s+o+h),time3,r,100)
+            time3 = time3 + r
         else:
             s = s*3+1
-            T.addNote(0,cha,val(s+o),time2,r,100)
-            T.addNote(0,cha,val(s+o+h),time2,r,100)
-            time2 = time2 + r
+            T.addNote(0,cha,val(s+o),time3,r,100)
+            T.addNote(0,cha,val(s+o+h),time3,r,100)
+            time3 = time3 + r
 
 #-------Piece-------
 
 k = 10 # maybe 9?
     
+temp = 159 
+T.addTempo(0,time,temp)
+
 for i in range(1,k+1):
     kol1(i)
+
+T.addTempo(0,time,temp+10)
 
 for i in range(k+1, 2*k):
     kol2(i, 3)
 
+T.addTempo(0,time,temp+20)
+
 for i in range(2*k, 3*k-3):
-    kol3(i, 4, -3, 60) 
+    kol3(i, 4, -3, 60, 0) 
 
 time = time + 1
-time2 = time 
+
+T.addTempo(0,time,temp+27)
 
 for i in range(3*k-1, 3*k+4):
-    kol3(i, 3, -4, 61)
-    kol4(i, 7, 1/3, 1)
-    time2 = time
-# add more harmony
-for i in range(3*k+4, 3*k+9):
-    kol3(i, 3, -4, 61)
-    # kol4(i, 7, 2/3, 1)
-    time2 = time
+    time2 = time 
+    kol3(i, 3, -4, 61, 0)
+    o = 33
+    time = time2
+    kol3(i, 7, -6, 61, 0)
+    o = 21
+    time3 = time2
+    kol4(i, 2, 1/3, 1)
+    time3 = time
+    o = 29
 
-for i in range(3*k+9, 3*k+14):
-    kol3(i, 3, -4, 61)
-    kol4(i, 7, 2/3, 1)
-    time2 = time
+for i in range(3*k+4, 3*k+9):
+    time2 = time 
+    kol3(i, 3, -4, 61, 0)
+    o = 33
+    time = time2
+    kol3(i, 7, -6, 61, 0)
+    # o = 21 FIX
+    # time = time2
+    # time3 = time
+    # kol4(i, 1/3, 1)
+    o = 29
+    # time3 = time
+
+time2 = time
+
+# for i in range(3*k+9, 3*k+14):
+#     kol3(i, 3, -4, 61)
+#     kol4(i, 7, 2/3, 1)
+#     time2 = time
 
 os.chdir('C:\\Users\\vatis\\OneDrive\\Documents\\Midi_Tapes\\Programmed_Pieces\\Kollatz_Musik')
 with open('Kollatz_Musik.mid', 'wb') as output_file:
